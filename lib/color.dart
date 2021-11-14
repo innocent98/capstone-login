c140c426f2318ce46b287f30d0c52076091ca94b
Victor Oluwatosin <adevictor98@gmail.com>
2021-11-14 18:59:46 +0100

    login completed

---
+++ lib/color.dart
@@ -0,0 +1,4 @@
+import 'dart:ui';
+
+final Color background = Color(0xFffDFEFE);
+final Color button = Color(0xFF00cc99);
---
+++ lib/login.dart
@@ -0,0 +1,266 @@
+import 'package:flutter/material.dart';
+import 'color.dart' as appColor;
+
+class MyLogin extends StatefulWidget {
+  const MyLogin({Key key}) : super(key: key);
+
+  @override
+  _MyLoginState createState() => _MyLoginState();
+}
+
+class _MyLoginState extends State<MyLogin> {
+  final _formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    final double screenHeight = MediaQuery.of(context).size.height;
+    final double screenWidth = MediaQuery.of(context).size.width;
+    return Container(
+      color: appColor.background,
+      child: SafeArea(
+        child: Scaffold(
+          backgroundColor: appColor.background,
+          body: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(10),
+              ),
+              Container(
+                height: screenHeight * 0.35,
+                width: screenWidth * 1,
+                child: Column(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(top: 30),
+                      height: screenHeight * 0.2,
+                      width: screenWidth * 0.45,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(100),
+                        image: DecorationImage(
+                          image: AssetImage("assets/img/lg.png"),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      "Welcome!",
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      "Login to your account to continue",
+                      style: TextStyle(
+                        color: Colors.grey,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Container(
+                    padding: const EdgeInsets.all(20),
+                    margin: const EdgeInsets.only(top: 20),
+                    child: Column(
+                      children: [
+                        Form(
+                          key: _formKey,
+                          child: Column(
+                            children: [
+                              TextFormField(
+                                decoration: InputDecoration(
+                                  labelText: "Email",
+                                  hintText: "adevictor98@gmail.com",
+                                  suffixIcon: Icon(
+                                    Icons.person,
+                                    color: appColor.button,
+                                  ),
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(
+                                      10,
+                                    ),
+                                  ),
+                                  focusedBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                      width: 1,
+                                      color: appColor.button,
+                                    ),
+                                    borderRadius: BorderRadius.circular(
+                                      10,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                height: 15,
+                              ),
+                              TextFormField(
+                                decoration: InputDecoration(
+                                  labelText: "password",
+                                  hintText: "adr#^/08",
+                                  suffixIcon: Icon(
+                                    Icons.lock,
+                                    color: appColor.button,
+                                  ),
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(
+                                      10,
+                                    ),
+                                  ),
+                                  focusedBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                      width: 1,
+                                      color: appColor.button,
+                                    ),
+                                    borderRadius: BorderRadius.circular(
+                                      10,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                height: 5,
+                              ),
+                              Container(
+                                alignment: Alignment.centerRight,
+                                child: Text(
+                                  "Forget Password?",
+                                  style: TextStyle(
+                                    color: Colors.grey,
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                height: 20,
+                              ),
+                              Container(
+                                width: screenWidth * 1,
+                                height: screenHeight * 0.05,
+                                child: ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                    primary: appColor.button,
+                                  ),
+                                  onPressed: () {},
+                                  child: Text(
+                                    "Login",
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        Container(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Container(
+                                height: screenHeight * 0.001,
+                                width: screenWidth * 0.2,
+                                color: Colors.grey,
+                              ),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              Text("OR"),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              Container(
+                                height: screenHeight * 0.001,
+                                width: screenWidth * 0.2,
+                                color: Colors.grey,
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        Container(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Container(
+                                padding: const EdgeInsets.all(8),
+                                height: screenHeight * 0.04,
+                                width: screenWidth * 0.2,
+                                decoration: BoxDecoration(
+                                  border: Border.all(color: Colors.grey),
+                                  borderRadius: BorderRadius.circular(
+                                    5,
+                                  ),
+                                ),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                      image: AssetImage("assets/img/gog.png"),
+                                      fit: BoxFit.contain,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                width: 20,
+                              ),
+                              Container(
+                                padding: const EdgeInsets.all(8),
+                                height: screenHeight * 0.04,
+                                width: screenWidth * 0.2,
+                                decoration: BoxDecoration(
+                                  border: Border.all(color: Colors.grey),
+                                  borderRadius: BorderRadius.circular(
+                                    5,
+                                  ),
+                                ),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                      image: AssetImage("assets/img/fb.png"),
+                                      fit: BoxFit.contain,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text("Don't have an account?"),
+                            SizedBox(
+                              width: 5,
+                            ),
+                            Text(
+                              "Sign up",
+                              style: TextStyle(
+                                  color: appColor.button,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
